93b2dbe4cd470ae78c4d9d5b397ad6dc1c595c3b
Nasrul Huda <nasrul2001@gmail.com>
2025-06-14 19:02:31 +0200

    [2566] [Time Beats: 100.00%] [Memory Beats: 11.69%] - LeetPush

---
+++ Maximum-Difference-by-Remapping-a-Digit.py
@@ -0,0 +1,25 @@
+import numpy as np
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        log_base_10 = np.log(num) / np.log(10)
+        T = pow(10, int(log_base_10))
+        seeA, seeB = False, False
+        a, b, x, y = -1, -1, 0, 0
+        t = T
+        while t >= 1:
+            d, num = divmod(num, t)
+            if not seeA and d != 9:
+                a = d
+                seeA = True
+                x += 9 * t
+            elif seeA and d == a:
+                x += 9 * t
+            else:
+                x += d * t
+            if not seeB and d != 0:
+                b = d
+                seeB = True
+            elif d != b:
+                y+=d*t
+            t //= 10
+        return x - y
